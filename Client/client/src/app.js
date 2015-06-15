5d11614ec635f8e0bd3493d3675d0a3ca0826a26
Brian <briancjliu@gmail.com>
2015-06-14 22:50:39 -0700

    Setting up for future shift to react

--- Client/client/src/app.js
+++ Client/client/src/app.js
@@ -1,1 +1,14 @@
-app.
+window.React = require('react');
+
+var App = React.createClass({
+  getInitialState: function( ){
+    return {
+
+    }
+  },
+  render: function(){
+    <div>
+    This is the React body
+    </div>
+  }
+})
